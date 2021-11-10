0a49f16e72f1dfc567a7f56daf0f44e42b272874
V Sai Karthik <92426409+VSaikarthik@users.noreply.github.com>
2021-11-10 02:17:46 -0800

    Delete program 10.py

--- program 10.py
+++
@@ -1,9 +0,0 @@
-n = int(input("Enter the n"))  
-for i in range(0, n):  
-        for j in range(0, i + 1):  
-            print("* ", end="")       
-        print()
-for i in range(n, 1, -1):
-    for j in range(1,i):
-        print("* ",end="")
-    print()
