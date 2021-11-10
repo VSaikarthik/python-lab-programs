04d10b95e14c7d9ab20a5baaecaff636e22f6445
V Sai Karthik <92426409+VSaikarthik@users.noreply.github.com>
2021-11-10 02:18:01 -0800

    Delete program 8.py

--- program 8.py
+++
@@ -1,8 +0,0 @@
-num1= float(input("enter the number 1:"))
-num2= float(input("enter the number 2:"))
-num3= float(input("enter the number 3:"))
-big = num1
-if(big<num2):big=num2
-if(big<num3):big=num3
-print("the Biggest number among the three is :",big)
-            
