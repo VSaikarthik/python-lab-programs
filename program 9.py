3f3712717361ef4814916e0c34951c18d839e7ab
V Sai Karthik <92426409+VSaikarthik@users.noreply.github.com>
2021-11-10 02:18:06 -0800

    Delete program 9.py

--- program 9.py
+++
@@ -1,6 +0,0 @@
-celsius=float(input("Enter the temperature in celcius:"))
-f=(celsius*1.8)+32
-print("Temperature in farenheit is:", round(f,1))
-fahrenheit = float(input("Enter Temperature in Fahrenheit: ")) 
-c = (fahrenheit-32)/1.8
-print("Temperature in Celsius is =", round(c,1))
